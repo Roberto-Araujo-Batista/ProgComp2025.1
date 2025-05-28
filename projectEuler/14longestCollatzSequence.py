0c05dd83d078364eb7de1d38ec0cbd1c25e36768
Roberto-Araujo-Batista <araujoroberto.contato@gmail.com>
2025-05-28 05:18:57 -0300

    Update 14longestCollatzSequence.py
    fiz a adição das variáveis para receber o maior número dos contadores e o maior número que possui os contadores.

--- projectEuler/14longestCollatzSequence.py
+++ projectEuler/14longestCollatzSequence.py
@@ -1,11 +1,12 @@
 #fazer um programa para ver a maior corrente de collatz sequence, abaixo de um milhão
 
 
-i =2
+i = 2
 limite = 10**6
 maior = 0
+maiorElementos = 0
 
-while i < 10:
+while i < 1000000:
     number = i
     contador = 1 
     while number > 1:
@@ -15,10 +16,10 @@ while i < 10:
             number = 3*number+1
         contador = contador +1
     #para receber o maior numero de sequencia
-    if maior < contador:
-        maior = contador
+    if maiorElementos < contador:
+        maior = i
+        maiorElementos = contador
     i = i+1
 
 
-print(number, maior)
-
+print(maior)
