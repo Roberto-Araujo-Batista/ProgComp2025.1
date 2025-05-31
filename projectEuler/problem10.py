cb6eab48c31d97ac2d205c1d2032d79c89b8ea8b
Roberto-Araujo-Batista <araujoroberto.contato@gmail.com>
2025-05-31 11:13:18 -0300

    Update problem10.py
    melhorando o código, ainda precisa colocar algumas condicionais para verificar os número primos muito grandes de forma mais rápida.

--- projectEuler/problem10.py
+++ projectEuler/problem10.py
@@ -1,20 +1,22 @@
 #sum of the primes numbers bellow two million
 
 #and now, i need to find the primes bellow a number
-limit = 2*10**6
-sum =0
+limit = 10000
+sum = 0
 
-for x in range(1,limit):
+for x in range(1,limit+1):
     number = x
     dividers = 0
-
-#first i need to find the primes
-    for divisor in range(1, number+1):
-        if number % divisor == 0:
-            dividers = dividers +1
+    #first i need to find the primes
+    divisor = 1
+    #the code is taking a lot of time to return the sum, so the fast way to do this is not operating all number bellow 
+    while divisor <= number and dividers <=2: #if dividers >2 the number is not prime
+            if number % divisor == 0:
+                dividers = dividers + 1
+            print(divisor)
+            divisor = divisor + 1
 
     if dividers ==2:
-        print(number)
         sum = sum + number
-    
-print(sum)
+print('a soma de todos os primos abaixo de ', limit, 'é: ', sum)
+
