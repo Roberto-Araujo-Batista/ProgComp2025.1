c423a1a221d4a5045165a64d751849c60e6cc55e
Roberto-Araujo-Batista <araujoroberto.contato@gmail.com>
2025-07-01 18:13:04 -0300

    Update problem52.py
    criada nova variável para receber numeroAtual

--- projectEuler/problem52.py
+++ projectEuler/problem52.py
@@ -18,11 +18,11 @@ while (len(lstnumero) != multiplicador):
     algnumero.sort() #algarismo do numero prontos para comparação
 
     #separar algarismos do numero multiplicado
-    contador = 1
+    contador = 2
     igual = True
-    while (contador < multiplicador) and igual:
-        aux = numero
-        aux = aux + numero
+    numAtual = numero * 2
+    while (contador <= multiplicador) and igual:
+        aux = numero * contador 
         algmulti = []
         while aux >0:
             algarismo = aux %10
@@ -32,12 +32,11 @@ while (len(lstnumero) != multiplicador):
         print(algmulti, algnumero,lstnumero)
     #comparar algarismos do numero multiplicado com algarismos do primeiro numero 
         if algnumero == algmulti:
-            lstnumero = lstnumero + [numero]
+            lstnumero = lstnumero + [numero * contador]
             print('eles são iguais', lstnumero)
         else:#preparando para compara o próximo número
-            print('eles não são iguais', numero)
+            print('eles não são iguais', numero * contador)
             numero = numero +1
             igual = False 
         contador = contador + 1
-
 
