b600abb86286fdcd8cc09386ca8202ad933545bd
Roberto-Araujo-Batista <araujoroberto.contato@gmail.com>
2025-07-10 12:55:50 -0300

    Add files via upload
    precisa encontrar a posição certa na lista "primos"

---
+++ projectEuler/problem7.py
@@ -0,0 +1,28 @@
+import random
+#fazer um código que liste os números primos e mostre o 10001 número primo
+
+#listar número primos
+
+
+#descobrir se um número é primo
+
+primos = []
+limite = 10002
+
+numero = 2
+while len(primos) <= limite:
+    divisor = 2
+    ehprimo = True
+
+    while divisor <= numero**(1/2) and ehprimo:
+        if numero %divisor == 0:
+            ehprimo = False
+        else:
+            divisor = divisor +1
+    
+    if ehprimo:
+        primos = primos +[numero]
+    
+    numero = numero +1
+
+print(len(primos), primos[0])
