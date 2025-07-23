b5561206b3ba7d9a90cd13de7e2d32cc59892cca
Roberto-Araujo-Batista <araujoroberto.contato@gmail.com>
2025-07-23 15:03:42 -0300

    Update arquivos.py
    feito até o item 4, sem os gráficos por enquanto

--- codigos/arquivos.py
+++ codigos/arquivos.py
@@ -1,25 +1,24 @@
 '''1) Quantos pedidos houve em cada data/hora/minuto?
 
-2) Qual o dia/hora com mais pedidos? 
+2) Qual o dia/hora com mais pedidos?
 
-3) Houve respostas diferentes do código 200? 
+3) Houve respostas diferentes do código 200?
    Quais e quantos? Faca um gráfico de barra
    com essa informação.
-   
+
 4) Gere um arquivo com nome "resposta_data.txt" em
    que cada linha contem a data e o número de pedidos
-   naquela data. Exiba, também, esta informação em 
+   naquela data. Exiba, também, esta informação em
    um gráfico de linhas.
-   
+
 5) Qual o IP fez mais pedidos em cada um dos dias do log?
 
-6) Salve um arquivo com a lista de IPs (um por linha) que 
-   fizeram pedidos ao servidor. O nome desse arquivo deve 
+6) Salve um arquivo com a lista de IPs (um por linha) que
+   fizeram pedidos ao servidor. O nome desse arquivo deve
    ser "ips.txt"'''
 
-
-#1 quantos pedidos houveram na mesma hora e minuto
-arquivo = open('apache.logs', 'r') #abrindo arquivo para leitura
+# 1 quantos pedidos houveram na mesma hora e minuto
+arquivo = open('apache.logs', 'r')  # abrindo arquivo para leitura
 contagemdata = dict()
 
 for linha in arquivo:
@@ -31,25 +30,48 @@ for linha in arquivo:
         contagemdata[data] = contagemdata[data] + 1
 arquivo.close()
 
-print(contagemdata)
+print('Quantos pedidos houveram no mesmo horário(H:min): ')
+for itens in contagemdata.items():
+    print(itens)
 
-#2 qual horário com mais acesso?
+# 2 qual horário com mais acesso?
 maior = 0
 for chave in contagemdata.keys():
     if contagemdata[chave] > maior:
         maiorchave = chave
         maior = contagemdata[chave]
-print('-'* 20)
+print('-' * 20)
 
 print(f'o horário com maior acesso foi: {maiorchave} \ncom: {contagemdata[maiorchave]}')
 
-
-
-#3 houve código diferente do 200? quais e quantos foram? gere um gráfico de barras
+# 3 houve código diferente do 200? quais e quantos foram? gere um gráfico de barras
 arquivo = open('apache.logs', 'r')
 
+codigos = dict()
 for linha in arquivo:
-    print(linha.split()[8])
+    codigo = linha.split()[8]
+
+    if codigo in codigos:
+        codigos[codigo] = codigos[codigo] + 1
+    else:
+        codigos[codigo] = 1
 
 
+print('-' *20)
+print(f'os códigos e a quantidade de vezes que eles aparecem no arquivo: \n{codigos}')
+print('-'*20)
+arquivo.close()
 
+#4 Gere um arquivo com nome "resposta_data.txt" em que cada linha contem a data e o número de pedidos naquela data.
+
+arquivo = open('apache.logs', 'r')
+pedidos = dict()
+
+for linha in arquivo:
+    data = linha.split()[3][1:12]
+    if not data in pedidos:
+        pedidos[data] = 1
+    else:
+        pedidos[data] = pedidos[data] + 1
+print('Quantidade de pedidos pela data:')
+print(pedidos)
